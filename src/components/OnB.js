8e9ed08f12b0729542082ef6046e1962bb26ce7a
connor <mcwvproductions@gmail.com>
2019-04-29 00:25:39 -0700

    basic functionality should be here; need to test with lights

---
+++ src/components/OnB.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+import Button from 'react-bootstrap/Button'
+
+class OnB extends Component {
+
+    on = () => {
+        alert('tes');
+        this.url = '172.28.219.225/api/1028d66426293e821ecfd9ef1a0731df'
+        Axios.put(this.url + '/lights/1/state', {
+          on : true
+        });
+    }
+
+    render() {
+        return(
+            <Button onClick={this.on}>TURN IT ON</Button>
+        )
+    }
+}
+
+export default OnB
